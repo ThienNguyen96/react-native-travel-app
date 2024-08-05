b869a69ec6c52f9e9f8a530f4edbe15e26c55f16
Thien Nguyen <doithua9669@gmail.com>
2024-08-06 00:16:43 +0700

    add calender

--- app/create-trip/select-dates.jsx
+++ app/create-trip/select-dates.jsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, TouchableOpacity, ToastAndroid } from 'react-native'
+import React, { useContext, useEffect } from 'react'
 import { useNavigation } from 'expo-router'
 import { Colors } from '../../constants/Colors';
 import CalendarPicker from 'react-native-calendar-picker';
+import { useState } from 'react';
+import moment from 'moment';
+import { CreateTripContext } from '../../context/CreateTripContext';
 
 export default function SelectDate() {
 
     const navigation = useNavigation();
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+    const {tripData, setTripData} = useContext(CreateTripContext);
 
     useEffect(() => {
         navigation.setOptions({
@@ -15,6 +21,28 @@ export default function SelectDate() {
             headerTitle: ''
         })
     }, []);
+
+    const onDateChange = (date, type) => {
+      console.log('DATE', date, 'TYPE', type);
+      type =='START_DATE' ? setStartDate(moment(date)) : setEndDate(moment(date));
+    }
+
+    const onDateSelectionContinue = () => {
+      if(!startDate && !endDate) {
+        ToastAndroid.show('Please select Start and End date', ToastAndroid.LONG);
+        return;
+      }
+
+      const totalNoOfDays = endDate.diff(startDate, 'days');
+      console.log(totalNoOfDays + 1);
+      
+      setTripData({
+        ...tripData,
+        startDate: startDate,
+        endDate: endDate,
+        totalNoOfDays: totalNoOfDays + 1
+      });
+    }
 
   return (
     <View style={{
@@ -29,9 +57,39 @@ export default function SelectDate() {
         marginTop: 20
       }}>Travel Dates</Text>
 
-      <View>
-        <CalendarPicker  />
+      <View style={{
+        marginTop: 30
+      }}>
+        <CalendarPicker 
+          onDateChange={onDateChange}
+          allowRangeSelection={true}
+          minDate={new Date()}
+          maxRangeDuration={5}
+          selectedRangeStyle={{
+            backgroundColor: Colors.PRIMARY
+          }}
+          selectedDayTextStyle={{
+            color: Colors.WHITE
+          }}
+        />
       </View>
+
+      <TouchableOpacity 
+          style={{
+            padding: 15,
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 15,
+            marginTop: 35
+          }}
+          onPress={onDateSelectionContinue}
+          >
+            <Text style={{
+                color: Colors.WHITE,
+                fontFamily: 'outfit-medium',
+                fontSize: 20,
+                textAlign: 'center'
+            }}>Continue</Text>
+        </TouchableOpacity>
     </View>
   )
 }
